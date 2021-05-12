8b7825c172c1378fa4925e36f873e89331218de2
guyerez7 <guyerez1999@gmail.com>
2021-05-12 19:48:07 +0300

    Just a little bit more
    things to do:
    1.check if using lists length is usefull and shortens the code significantly.
    2. check if there are no missing nodes in the end.
    3. organize and finish the errors code.

--- hw1dry.c
+++ hw1dry.c
@@ -15,67 +15,83 @@ typedef enum {
     
 int getListLength(Node list);
 bool isListSorted(Node list);
-
-
-
-
-
 Node createNode(int x, ErrorCode* error_code);
-Node finishTailEnd(Node list, Node mergedlist){
-    while(list->next != NULL){
-        mergedlist->x = list->x;
-        mergedlist->next = list->next;
-        
-        mergedlist = mergedlist->next;
-        list = list->next;
-    }
-    mergedlist->next = NULL;
-    
-    return mergedlist;
-}
+int placeAndPromote(int xx, Node* ptr_list, Node merged_list);
+
 
 Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
 {
     if(getListLength(list1) == 0 || getListLength(list2) == 0){    
-        return error_code = NULL_ARGUMENT;
+        error_code = NULL_ARGUMENT;
+        return NULL;
     }
     if(isListSorted(list1) == false || isListSorted(list2) == false){
-        return error_code = UNSORTED_ERROR;
+        error_code = UNSORTED_ERROR;
+        return NULL;
     }
 
-    //(*ptrlist1)->next;
-    Node *ptrlist1 = &list1, *ptrlist2 = &list2, *ptrnext = NULL;
-    //Node mergedlist
+    Node *ptr_list1 = &list1, *ptr_list2 = &list2;
+    Node *ptr_start = getMin((*ptr_list1)->x, (*ptr_list2)->x);
+   
     int xx = 0;
-
-	while((*ptrlist1)->next != NULL && (*ptrlist2)->next != NULL) {
+	while((*ptr_list1)->next != NULL && (*ptr_list2)->next != NULL) {
         
         Node merged_list = createNode(xx,error_code);
-        if((*ptrlist1)->x <= (*ptrlist2)->x) {
-            xx = (*ptrlist1)->x;
-            merged_list->next = *ptrlist1;
-            *ptrlist1 = (*ptrlist1)->next; 
+        if(merged_list == NULL){
+            return NULL;
+        }
+
+        if((*ptr_list1)->x <= (*ptr_list2)->x) { 
+            xx = placeAndPromote(xx, ptr_list1, merged_list);
         }
         else{
-            xx = (*ptrlist2)->x;
-            merged_list->next = *ptrlist2;
-            *ptrlist2 = (*ptrlist2)->next;
+            xx = placeAndPromote(xx, ptr_list2, merged_list);
         }
         
 	} 
-    
-    finishTailEnd() // we must define and use (to fill merged tail)
+    // filling the tail
+    if((*ptr_list1)->next == NULL){
+        while((*ptr_list1)->next != NULL){
+            Node merged_list = createNode(xx,error_code);
+            if(merged_list == NULL){
+                return NULL;
+            }
+            xx = placeAndPromote(xx, ptr_list1, merged_list);
+        }
+    }
+    else{
+         while((*ptr_list2)->next != NULL){
+            Node merged_list = createNode(xx,error_code);
+            if(merged_list == NULL){
+                return NULL;
+            }
+            xx = placeAndPromote(xx, ptr_list2, merged_list); 
+        }
+    }
+    error_code = SUCCESS;
+    return *ptr_start;
 }
 
 Node createNode(int x, ErrorCode* error_code){
     Node merged_list = malloc(sizeof(Node));
-    if(merged_list == NULL) {
+    if(merged_list == NULL) { // we must return NULL also in th main function
         error_code = MEMORY_ERROR;
         return NULL;
     }
     merged_list->x = x;
     merged_list->next = NULL;
-    error_code = SUCCESS;    //  check if really necessery
     return merged_list;
 }
 
+int placeAndPromote(int xx, Node* ptr_list, Node merged_list){
+    xx = (*ptr_list)->x;
+    merged_list->next = *ptr_list;
+    *ptr_list = (*ptr_list)->next; 
+    return xx;
+}
+
+
+int getMin(int num1, int num2)
+{
+    return (num1<=num2) ? num1 : num2;
+}
--- hw1errors.c
+++ hw1errors.c
@@ -5,16 +5,16 @@ char* foo(char* str, int* x) { // x is a shortened name that is not allowed (2) 
     char* str2;
     int i;
     x = strlen(str); // x is a pointer which recives a value without dereferencing (we fill its place not his value) (1)
-    str2 = malloc(*x); // sizeof() is missing (1) + malloc return value isn't checked (1/2) 
+    str2 = malloc(*x); // sizeof() is missing (1) + malloc return value isn't checked (1/2) +  malloc reads junk value
     for (i = 0; i < *x; i++)
-        str2[i] = str[*x - i]; // str[1] will never be copied (1) + must put {,} after a for loop (2)
-    if (*x % 2 == 0) {
+        str2[i] = str[*x - i]; // str[0] will never be copied (1) + must put {,} after a for loop (2)
+    if (*x % 2 == 0) { 
         printf("%s", str);
     }
     if (*x % 2 != 0)
     {
         printf("%s", str2);
     }
-    return str2; // 'free' is missing (1) 
+    return str2;  
 }
 
